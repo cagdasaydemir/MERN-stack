affe4a44c7e1ecb8cd3e409c4b7c4c0186f7cb54
cagdasaydemir <ac.cagdasaydemir@gmail.com>
2022-11-08 20:07:52 +0300

    fix typo

--- restaurant-reviews/backend/api/reviews.controller.js
+++ restaurant-reviews/backend/api/reviews.controller.js
@@ -54,7 +54,7 @@ export default class ReviewsController {
 
   static async apiDeleteReview(req, res, next) {
     try {
-      const reviewId = req.query._id;
+      const reviewId = req.query.id;
       const userId = req.body.user_id;
       console.log(reviewId);
 
