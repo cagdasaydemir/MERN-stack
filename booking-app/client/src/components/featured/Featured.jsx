cbd7a8929732462823a3d6a5706f0c9a10e7da8a
cagdasaydemir <ac.cagdasaydemir@gmail.com>
2022-11-10 18:44:42 +0300

    homePage and searchPage

---
+++ booking-app/client/src/components/featured/Featured.jsx
@@ -0,0 +1,31 @@
+import "./featured.css"
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img className="featuredImg" src="//bstatic.com/xdata/images/xphoto/1182x887/170335341.jpg?k=4d95afd5a003c6f1ee976ac685a922ecec2e3d2b9bab29501b127d79bb5a74fd&amp;o=?size=S" />
+        <div className="featuredTitles">
+          <h1>Sydney</h1>
+          <h2>123 Properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img className="featuredImg" src="//bstatic.com/xdata/images/xphoto/1182x887/170335341.jpg?k=4d95afd5a003c6f1ee976ac685a922ecec2e3d2b9bab29501b127d79bb5a74fd&amp;o=?size=S" />
+        <div className="featuredTitles">
+          <h1>Sydney</h1>
+          <h2>123 Properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img className="featuredImg" src="//bstatic.com/xdata/images/xphoto/1182x887/170335341.jpg?k=4d95afd5a003c6f1ee976ac685a922ecec2e3d2b9bab29501b127d79bb5a74fd&amp;o=?size=S" />
+        <div className="featuredTitles">
+          <h1>Sydney</h1>
+          <h2>123 Properties</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
---
+++ booking-app/client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,50 @@
+import "./featuredProperties.css"
+
+const FeaturedProperties = () => {
+  return (
+    <div className="fp">
+      <div className="fpItem">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/max500/74529578.jpg?k=a7fcefd47d7271daf44f6ce78a215b9505f9f8e5cac3af093b78b9c489fd8461&amp;o="/>
+      <span className="fpName">Sugar Loft Apartments</span>
+      <span className="fpCity">Rio de Janerio</span>
+      <span className="fpPrice">Starting from TL 709</span>
+      <div className="fpRating">
+        <button>9.0</button>
+        <span>Wonderful</span>
+      </div>
+    </div>
+      <div className="fpItem">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/max500/74529578.jpg?k=a7fcefd47d7271daf44f6ce78a215b9505f9f8e5cac3af093b78b9c489fd8461&amp;o="/>
+      <span className="fpName">Sugar Loft Apartments</span>
+      <span className="fpCity">Rio de Janerio</span>
+      <span className="fpPrice">Starting from TL 709</span>
+      <div className="fpRating">
+        <button>9.0</button>
+        <span>Wonderful</span>
+      </div>
+    </div>
+    <div className="fpItem">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/max500/74529578.jpg?k=a7fcefd47d7271daf44f6ce78a215b9505f9f8e5cac3af093b78b9c489fd8461&amp;o="/>
+      <span className="fpName">Sugar Loft Apartments</span>
+      <span className="fpCity">Rio de Janerio</span>
+      <span className="fpPrice">Starting from TL 709</span>
+      <div className="fpRating">
+        <button>9.0</button>
+        <span>Wonderful</span>
+      </div>
+    </div>
+    <div className="fpItem">
+      <img src="https://cf.bstatic.com/xdata/images/hotel/max500/74529578.jpg?k=a7fcefd47d7271daf44f6ce78a215b9505f9f8e5cac3af093b78b9c489fd8461&amp;o="/>
+      <span className="fpName">Sugar Loft Apartments</span>
+      <span className="fpCity">Rio de Janerio</span>
+      <span className="fpPrice">Starting from TL 709</span>
+      <div className="fpRating">
+        <button>9.0</button>
+        <span>Wonderful</span>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default FeaturedProperties
