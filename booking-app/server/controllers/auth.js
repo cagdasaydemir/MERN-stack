4d5f9d1235dd19e84db68dd1503aca06dcda3b38
cagdasaydemir <ac.cagdasaydemir@gmail.com>
2022-11-14 17:04:54 +0300

    authorization w/jwt

--- booking-app/server/controllers/auth.js
+++ booking-app/server/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   try {
@@ -23,7 +24,6 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    console.log(user.username)
     if (!user) return next(createError(404, "User not found"));
 
     const isPasswordCorrect = await bcrypt.compare(
@@ -33,7 +33,16 @@ export const login = async (req, res, next) => {
     if (!isPasswordCorrect)
       return next(createError(400, "Wrong password or username !"));
 
-    res.status(200).json(user);
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json({ ...otherDetails });
   } catch (err) {
     next(err);
   }
